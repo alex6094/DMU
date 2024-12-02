e136568d6d973ac5f9f808fb2b36dfebb827988b
Alexander Steen-Jørgensen <129056493+alex6094@users.noreply.github.com>
2024-12-02 16:12:01 +0100

    Created XML mock exam that takes "car.xml", reads it, adds car to a list and writes a new XML file with the list of cars

--- XML/CarXML/Program.cs
+++ XML/CarXML/Program.cs
@@ -1,5 +1,5 @@
-﻿
-using System.Xml;
+﻿using System.Xml;
+using System.Xml.Linq;
 
 namespace DMU.XML.CarXML
 {
@@ -7,34 +7,72 @@ namespace DMU.XML.CarXML
     {
         static void Main(string[] args)
         {
-            string filename = "C:\\Users\\alexa\\OneDrive\\Desktop\\Exam\\CarXML\\cars.xml";
-            string newFilename = "C:\\Users\\alexa\\OneDrive\\Desktop\\Exam\\CarXML\\newcars.xml";
+            //Creates new list for cars
+            List<Car> cars = new List<Car>();
 
-            try {
-                XmlReader reader = XmlReader.Create(filename);
+            //Creates XmlReader, that takes the filepath as parameter
+            XmlReader reader = XmlReader.Create("C:\\Users\\Alexander\\RiderProjects\\DMU\\XML\\CarXML\\cars.xml");
 
-                ReadXml(reader);
-
-                reader.Close();
-            }
-            catch (IOException) {
-                Console.WriteLine("Could not open file: " + filename);
-            }
-        }
-        public static void ReadXml(XmlReader reader)
-        {
+            //While there are cars in the list, the reader continues
             while (reader.Read())
             {
-                if (reader.ReadToDescendant("car", "http://www.fkj.dk/cars.xml"))
+                //Checks if the startelement is "car"
+                if (reader.IsStartElement("car"))
                 {
-                    do
-                    {
-                        reader.Read();
-                        Console.WriteLine(reader.ReadContentAsString());
-                    } while (reader.ReadToNextSibling("car", "http://www.fkj.dk/cars.xml"));
+                    //If the start element is "car", the reader takes the attribute (value) of "name", which in this case is the model name
+                    string name = reader.GetAttribute("name");
+
+                    //After the name has been read, ReadToDescendant moves the reader to the next element that is indented
+                    reader.ReadToDescendant("cylinders");
+                    
+                    //When the reader has found the int for cylinders, we use the reader to set the value of cylinders
+                    int cylinders = reader.ReadElementContentAsInt();
+
+                    //Here we use ReadToNextSibling to get the next element at the same indentation level
+                    reader.ReadToNextSibling("country");
+                    string country = reader.ReadElementContentAsString();
+                    
+                    //Create a new car with the values from the XML file
+                    Car car = new Car(name, cylinders, country);
+                    
+                    //Add the new car to the cars list
+                    cars.Add(car);
                 }
             }
-        }
+            reader.Close();
+            
+            //Create new 
+            XElement xCars = new XElement("cars");
 
+            foreach (Car car in cars)
+            {
+                Console.WriteLine(car.ToString());
+
+                XElement xCar = new XElement("car", new XAttribute("name", $"{car.Name}"),
+                                                        new XElement("cylinders", $"{car.Cylinders}"),
+                                                        new XElement("country", $"{car.Country}"));
+                xCars.Add(xCar);
+            }
+            string fileName = "C:\\Users\\Alexander\\RiderProjects\\DMU\\XML\\CarXML\\newcars.xml";
+            try
+            {
+                using(FileStream fs = new FileStream(fileName, FileMode.Create))
+                    using (StreamWriter sw = new StreamWriter(fs))
+                    using (XmlTextWriter xmlWriter = new XmlTextWriter(sw))
+                    {
+                        xmlWriter.Formatting = Formatting.Indented;
+                        xmlWriter.Indentation = 4;
+                        
+                        //xmlWriter.WriteStartDocument(); Can be used here to write the XML version and UTF encoding in the start of the new XML file
+                        
+                        xCars.WriteTo(xmlWriter);
+                    }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
     }
 }
