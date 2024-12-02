8174f32f4748006adbd11743d31bbeb2664ceacd
Alexander Steen-Jørgensen <alexander@steen-joergensen.dk>
2024-12-02 14:36:01 +0100

    Created Car.cs, cars.xml and Program.cs

---
+++ XML/CarXML/Car.cs
@@ -0,0 +1,22 @@
+﻿namespace DMU.XML.CarXML
+{
+    public class Car
+    {
+        public string Name { get; set; }
+        public int Cylinders { get; set; }
+        public string Country { get; set; }
+
+        public Car(string name, int cylinders, string country)
+        {
+            this.Name = name;
+            this.Cylinders = cylinders;
+            this.Country = country;
+        }
+
+        public override string ToString()
+        {
+            string info = $"Name: {Name}, cylinders: {Cylinders}, country: {Country}";
+            return info;
+        }
+    }
+}
--- XML/CarXML/Program.cs
+++ XML/CarXML/Program.cs
@@ -1,6 +1,40 @@
-﻿namespace DMU.XML.CarXML;
+﻿
+using System.Xml;
 
-public class Program
+namespace DMU.XML.CarXML
 {
-    
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = "C:\\Users\\alexa\\OneDrive\\Desktop\\Exam\\CarXML\\cars.xml";
+            string newFilename = "C:\\Users\\alexa\\OneDrive\\Desktop\\Exam\\CarXML\\newcars.xml";
+
+            try {
+                XmlReader reader = XmlReader.Create(filename);
+
+                ReadXml(reader);
+
+                reader.Close();
+            }
+            catch (IOException) {
+                Console.WriteLine("Could not open file: " + filename);
+            }
+        }
+        public static void ReadXml(XmlReader reader)
+        {
+            while (reader.Read())
+            {
+                if (reader.ReadToDescendant("car", "http://www.fkj.dk/cars.xml"))
+                {
+                    do
+                    {
+                        reader.Read();
+                        Console.WriteLine(reader.ReadContentAsString());
+                    } while (reader.ReadToNextSibling("car", "http://www.fkj.dk/cars.xml"));
+                }
+            }
+        }
+
+    }
 }
